bc5b22dbeaaaaca806dc80e12e05d2fbbc9ff5df
MariuszKaminski <82467326+MariuszKaminski@users.noreply.github.com>
2022-03-27 03:12:12 +0100

    Create test database

---
+++ person.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+db = mysql.connector.connect(
+    host='127.0.0.1',
+    user='mario',
+    password='123password',
+    database='testdatabase'
+    )
+
+cur = db.cursor()
+
+cur.execute('SELECT * FROM person')
+
+for x in cur:
+    print(x)
+
+#db.commit()
+
+#db.close()
